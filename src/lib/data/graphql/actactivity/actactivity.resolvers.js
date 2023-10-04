409754d96b3e3e0049f0ec337ce1942cb37a3bea
Chris Mattinson <cmattinson@method1software.com>
2023-10-04 12:32:05 -0600

    feat: fully searchable activity browse

--- src/lib/data/graphql/actactivity/actactivity.resolvers.js
+++ src/lib/data/graphql/actactivity/actactivity.resolvers.js
@@ -1,29 +1,46 @@
 module.exports = {
     Query: {
         actactivities: (instance, args, context, info) => {
-            console.log(instance);
             let query = {};
-            let includes = [];
+            let priorityQuery = { required: false };
+            let typeQuery = { required: false };
+            let statusQuery = { required: false };
+            let projectQuery = { required: false };
 
             if (args) {
                 query = context.constructQuery(args.filter);
+
+                if (args.actpriority) {
+                    priorityQuery.where = context.constructQuery(args.actpriority);
+                    priorityQuery.required = true;
+                }
+
+                if (args.acttype) {
+                    typeQuery.where = context.constructQuery(args.acttype);
+                    typeQuery.required = true;
+                }
+
+                if (args.actstatus) {
+                    statusQuery.where = context.constructQuery(args.actstatus);
+                    statusQuery.required = true;
+                }
+
+                if (args.actproject) {
+                    projectQuery.where = context.constructQuery(args.actproject);
+                    projectQuery.required = true;
+                }
             }
 
-            return context.models.actactivity
-                .findAll({
-                    logging: console.log,
-                    where: { ...query, actpriorityid: { [context.Op.not]: null } },
-                    limit: parseInt(args.limit) ? parseInt(args.limit) : 100,
-                    include: [
-                        { model: context.models.actpriority, as: "actpriority" },
-                        { model: context.models.actstatus, as: "actstatus" },
-                        { model: context.models.acttype, as: "acttype" },
-                        { model: context.models.actproject, as: "actproject" },
-                    ],
-                })
-                .then((data) => {
-                    return data;
-                });
+            return context.models.actactivity.findAll({
+                where: { ...query },
+                limit: parseInt(args.limit) ? parseInt(args.limit) : 100,
+                include: [
+                    { model: context.models.actpriority, as: "actpriority", ...priorityQuery },
+                    { model: context.models.actstatus, as: "actstatus", ...statusQuery },
+                    { model: context.models.acttype, as: "acttype", ...typeQuery },
+                    { model: context.models.actproject, as: "actproject", ...projectQuery },
+                ],
+            });
         },
     },
 };
